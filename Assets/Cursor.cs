b26e2175370fdee3c7daf2c2d29ae5783a8f8797
Henrik Raitasola <henrik.raitasola@gmail.com>
2017-05-27 18:48:34 +0300

    Disallow laser by default

--- Assets/Cursor.cs
+++ Assets/Cursor.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.VR.WSA.Input;
 
 public class Cursor : MonoBehaviour {
-    private bool isLaserAllowed = true;
+    private bool isLaserAllowed = false;
     GestureRecognizer recognizer;
 
 	// Use this for initialization
